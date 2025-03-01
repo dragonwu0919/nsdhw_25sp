f3c89b308b0f75ff7ce203ee82bc80bc6fc05f64
dragonwu0919 <dragonwu0919@gmail.com>
2025-03-01 23:33:11 +0800

    modify the python script

--- hw1/dragonwu0919/countline.py
+++ hw1/dragonwu0919/countline.py
@@ -2,14 +2,28 @@
 
 import sys
 import os.path
+import subprocess
+
+# prevent from recursively calling script ifself
+if "--no-reexec" not in sys.argv:
+    python_bin = os.getenv("PYTHON_BIN")
+    if python_bin: # python_bin specified 
+        if not os.path.exists(python_bin) or not python_bin:
+            sys.stderr.write("Error: Specified Python binary '{}' not found\n".format(python_bin))
+            sys.exit(1)
+        else: 
+            subprocess.call([python_bin] + sys.argv + ["--no-reexec"])
+            sys.exit(0)
 
 
-if len(sys.argv) < 2:
+# count line
+args = [arg for arg in sys.argv if arg != "--no-reexec"]
+if len(args) < 2:
     sys.stdout.write('missing file name\n')
-elif len(sys.argv) > 2:
+elif len(args) > 2:
     sys.stdout.write('only one argument is allowed\n')
 else:
-    fname = sys.argv[1]
+    fname = args[1]
     if os.path.exists(fname):
         with open(fname) as fobj:
             lines = fobj.readlines()
